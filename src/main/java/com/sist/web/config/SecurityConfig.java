f454345e3bd740c1af164c3b2374da223dd3cbf6
hare211 <blackhole2748@gmail.com>
2025-06-11 12:51:49 +0900

    api 요청 security 에서 제외

--- src/main/java/com/sist/web/config/SecurityConfig.java
+++ src/main/java/com/sist/web/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.sist.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -10,5 +12,11 @@ public class SecurityConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests(auth -> auth.requestMatchers("/api/**").permitAll().anyRequest().authenticated()).formLogin();
+		return http.build();
 	}
 }
